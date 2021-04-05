70b36158ddf5f7b6baf6141229fd92fd7933b0a3
Destoky <adestoky@gmail.com>
2021-04-05 22:49:26 +0200

    Menu personnaliser

--- public/contact.php
+++ public/contact.php
@@ -1,9 +1,8 @@
 <?php ob_start();?>
 
-  <h2>On test</h2>
+  <h2>Page de contact</h2>
   <p>un paragraphe</p>
-  <button class="btn btn-success">Valider</button>
-
+  
 <?php 
   $content = ob_get_clean();
   require "partials/template.php";
