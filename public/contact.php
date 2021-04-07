668608d8382bc2633b88aa5d9cdf6f94e9ae2d19
Destoky <adestoky@gmail.com>
2021-04-07 12:59:56 +0200

    Contact formulaire

--- public/contact.php
+++ public/contact.php
@@ -1,23 +1,49 @@
 <?php ob_start();?>
 
-<div class="container text-center" id="contact">
+<div class="container text-center" >
   <h2 class="m-3">Mes infos personnelles</h2>
-  <table class="table table-borderless">
-    <thead>
-      <tr>
-        <td><img src="../images/facebook.png" width="100px"></td>
-        <td><img src="../images/github.png" width="100px"></td>
-        <td><img src="../images/linkedin.jpg" width="100px"></td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Facebook</td>
-        <td>Github</td>
-        <td>Linkedin</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-responsive">
+    <table class="table table-borderless">
+      <thead>
+        <tr>
+          <td><img src="../images/facebook.png" width="100px"></td>
+          <td><img src="../images/github.png" width="100px"></td>
+          <td><img src="../images/linkedin.jpg" width="100px"></td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Facebook</td>
+          <td>Github</td>
+          <td>Linkedin</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<div class="container text-center col-12 col-md-6 mt-4">
+  <h2 class="m-3">Formulaire de contact</h2>
+  <form method="POST" action="">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Adresse Email</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" name="mail" placeholder="Entrez votre mail">
+    </div>
+    <div class="form-group">
+      <label for="objet">Objet du mail</label>
+      <select class="form-control" id="objet" name="objet">
+        <option disabled selected="selected">Choisir</option>
+        <option value="question">Question</option>
+        <option value="remarque">Remarque</option>
+        <option value="autre">Autres</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="message">Votre Message</label>
+      <textarea class="form-control" id="message" name="message" rows="4" placeholder="Insérez votre message"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+  </form>
 </div>
 
 <?php 
